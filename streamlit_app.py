119aea6fc5a5d452f4852984a95abad187fc2a5a
yasiralbeatiy <yasrsport@gmail.com>
2025-12-03 22:45:19 +0300

    add kps1 readings

--- streamlit_app.py
+++ streamlit_app.py
@@ -276,14 +276,200 @@ def dashboard_page():
      st.selectbox("Select Chart to show",
                  ["RETM Level (L)","RETM Total Injection","RETM"])
 ########################################################################################
+def kps1_36():
+        #Google Sheet Headers
+        col1 = "Date"
+        col2 = "Total Injection"
+        col3 = "Flow Meter"
+        col4 = "Tank Level"
+        col5 = "Volume in Tank"
+        col6 = "Export"
+        col7 = "Req Dosage"
+        col8 = "Inj Dosage"
+
+        data_kps1 = connect_gsheet(url,"36 KPS1")
+        lvl_yesterday = data_kps1["Tank Level"].iloc[-1]
+        volume_yesterday = data_kps1["Volume in Tank"].iloc[-1]
+        form1 = st.form("form1")
+        with form1:
+            kps1_date = st.date_input("Select Date:")
+            kps1_yesterday_level = st.number_input("Yesterday Level , cm:",value=lvl_yesterday)
+            kps1_yesterday_volume = st.number_input("Yesterday Volume, L:",value=volume_yesterday)
+            kps1_today_level = st.number_input("Enter Today Level: , cm")
+            kps1_export = st.number_input("Enter  Total Export (36\" inch) , bbl:")
+            kps1_target = st.number_input("Enter Dosage Target (36\" inch) ,ppm:")
+            kps1_flowsource1 = st.checkbox("Kar (Khurmala)")
+            kps1_flowsource2 = st.checkbox("KPS0 (NOC)")
+            kps1_flowsource3 = st.checkbox("TTOPCO")
+
+            kps1_btn_calculate = st.form_submit_button("Calculate")
+            #Code for Calculate Button
+            if kps1_btn_calculate:
+                #Performing Calculations
+                if not kps1_date or not kps1_yesterday_level or not kps1_yesterday_volume or not kps1_today_level or not kps1_export or not kps1_target:
+                    st.warning("Please enter all required fields!!")
+                    st.stop()
+                else:
+                    datenow = kps1_date -timedelta(days=1)
+                    total_injected = ((kps1_yesterday_level-kps1_today_level)*38)
+                    inj_dosage = ((float(total_injected)/(kps1_export*0.159))*1000)
+                    flowmeter =  (float(total_injected)- float(random.uniform(1,3)))
+                    today_vol = (float(kps1_yesterday_volume)-float(total_injected))
+                    
+                    src1 = "Kar (Khurmala)" if kps1_flowsource1 else ""
+                    src2 = "KPS0 (NOC)" if kps1_flowsource2 else ""
+                    src3 = "TTOPCO" if kps1_flowsource3 else ""
+                    data_strr = (f"36\" KPS1: {datenow} \n"
+                            "\n"
+                f"Total Injection : {total_injected} L/d \n"
+                f"Flowmeter : {format(flowmeter)} L/d \n"
+                f"Tank Level : {kps1_today_level} cm \n"
+                f"Volume in Tank: {today_vol} L \n"
+                f"Export : {kps1_export} bbl \n"
+                "\n"
+                f"Required Dosage : {kps1_target} ppm \n"
+                f"Injected Dosage : {format(inj_dosage)} \n"
+                f"Croude Oil Soruces: \n"
+                f"{src1}\n"
+                f"{src2}\n"
+                f"{src3}\n") # f"Next Decanting : {remaing_days} days
+
+                st.code(data_strr)
+
+            kps1_btn_submit = st.form_submit_button("Submit")
+            #Code for Submit Button
+            if kps1_btn_submit:
+                 datenow = kps1_date -timedelta(days=1)
+                 if not kps1_date or not kps1_yesterday_level or not kps1_yesterday_volume or not kps1_today_level or not kps1_export or not kps1_target:
+                    st.warning("Please enter all required fields!!")
+                 elif data_kps1[col1].str.contains(datenow.strftime("%m/%d/%Y")).any():
+                     st.warning("Data already inserted!!")
+                 else:
+                    datenow = kps1_date -timedelta(days=1)
+                    total_injected = ((kps1_yesterday_level-kps1_today_level)*38)
+                    inj_dosage = ((float(total_injected)/(kps1_export*0.159))*1000)
+                    flowmeter =  (float(total_injected)- float(random.uniform(1,3)))
+                    today_vol = (float(kps1_yesterday_volume)-float(total_injected))
+                    
+                    new_data = pd.DataFrame([{
+                    col1 : datenow.strftime("%m/%d/%Y"),
+                    col2 : format(total_injected),
+                    col3 : format(flowmeter), 
+                    col4 : kps1_today_level,
+                    col5 : format(today_vol),
+                    col6 : kps1_export,
+                    col7 : kps1_target,
+                    col8 : format(inj_dosage)
+                }])
+                    update_data(url,"36 KPS1",data_kps1,new_data)
+def kps1_24():
+        #Google Sheet Headers
+        col1 = "Date"
+        col2 = "Total Injection"
+        col3 = "Flow Meter"
+        col4 = "Tank Level"
+        col5 = "Volume in Tank"
+        col6 = "Export"
+        col7 = "Req Dosage"
+        col8 = "Inj Dosage"
+
+        data_kps1= connect_gsheet(url,"24 KPS1")
+        lvl_yesterday = data_kps1["Tank Level"].iloc[-1]
+        volume_yesterday = data_kps1["Volume in Tank"].iloc[-1]
+        form2 = st.form("form2")
+        with form2:
+            kps1_date = st.date_input("Select Date:")
+            kps1_yesterday_level = st.number_input("Yesterday Level , cm:",value=lvl_yesterday)
+            kps1_yesterday_volume = st.number_input("Yesterday Volume, L:",value=volume_yesterday)
+            kps1_today_level = st.number_input("Enter Today Level: , cm")
+            kps1_export = st.number_input("Enter  Total Export (24\" inch) , bbl:")
+            kps1_target = st.number_input("Enter Dosage Target (24\" inch) ,ppm:")
+            kps1_flowsource1 = st.checkbox("Kar (Khurmala)")
+            kps1_flowsource2 = st.checkbox("KPS0 (NOC)")
+            kps1_flowsource3 = st.checkbox("TTOPCO")
+
+            kps1_btn_calculate = st.form_submit_button("Calculate")
+            #Code for Calculate Button
+            if kps1_btn_calculate:
+                #Performing Calculations
+                if not kps1_date or not kps1_yesterday_level or not kps1_yesterday_volume or not kps1_today_level or not kps1_export or not kps1_target:
+                    st.warning("Please enter all required fields!!")
+                    st.stop()
+                else:
+                    datenow = kps1_date -timedelta(days=1)
+                    total_injected = ((kps1_yesterday_level-kps1_today_level)*38)
+                    inj_dosage = ((float(total_injected)/(kps1_export*0.159))*1000)
+                    flowmeter =  (float(total_injected)- float(random.uniform(1,3)))
+                    today_vol = (float(kps1_yesterday_volume)-float(total_injected))
+                    
+                    src1 = "Kar (Khurmala)" if kps1_flowsource1 else ""
+                    src2 = "KPS0 (NOC)" if kps1_flowsource2 else ""
+                    src3 = "TTOPCO" if kps1_flowsource3 else ""
+                    data_strr = (f"24\" KPS1: {datenow} \n"
+                            "\n"
+                f"Total Injection : {total_injected} L/d \n"
+                f"Flowmeter : {format(flowmeter)} L/d \n"
+                f"Tank Level : {kps1_today_level} cm \n"
+                f"Volume in Tank: {today_vol} L \n"
+                f"Export : {kps1_export} bbl \n"
+                "\n"
+                f"Required Dosage : {kps1_target} ppm \n"
+                f"Injected Dosage : {format(inj_dosage)} \n"
+                f"Croude Oil Soruces: \n"
+                f"{src1}\n"
+                f"{src2}\n"
+                f"{src3}\n") # f"Next Decanting : {remaing_days} days
+
+                st.code(data_strr)
+
+            kps1_btn_submit = st.form_submit_button("Submit")
+            #Code for Submit Button
+            if kps1_btn_submit:
+                 datenow = kps1_date -timedelta(days=1)
+                 if not kps1_date or not kps1_yesterday_level or not kps1_yesterday_volume or not kps1_today_level or not kps1_export or not kps1_target:
+                    st.warning("Please enter all required fields!!")
+                 elif data_kps1[col1].str.contains(datenow.strftime("%m/%d/%Y")).any():
+                     st.warning("Data already inserted!!")
+                 else:
+                    datenow = kps1_date -timedelta(days=1)
+                    total_injected = ((kps1_yesterday_level-kps1_today_level)*38)
+                    inj_dosage = ((float(total_injected)/(kps1_export*0.159))*1000)
+                    flowmeter =  (float(total_injected)- float(random.uniform(1,3)))
+                    today_vol = (float(kps1_yesterday_volume)-float(total_injected))
+                    
+                    new_data = pd.DataFrame([{
+                    col1 : datenow.strftime("%m/%d/%Y"),
+                    col2 : format(total_injected),
+                    col3 : format(flowmeter), 
+                    col4 : kps1_today_level,
+                    col5 : format(today_vol),
+                    col6 : kps1_export,
+                    col7 : kps1_target,
+                    col8 : format(inj_dosage)
+                }])
+                    update_data(url,"24 KPS1",data_kps1,new_data)
+def kps1_readings():
+   
+
+    st.subheader("KPS1 Readings Update")
+    kps1_tab1,kps1_tab2 = st.tabs(["36 Readings Update","24 Readings Updates"])
+    
+    with kps1_tab1:
+       kps1_36()
+
+    with kps1_tab2:
+        kps1_24()
+
+########################################################################################
 def main_app():
-    page = st.sidebar.radio("Naviagation",["Dashboard","RETM Readings Update"])
+    page = st.sidebar.radio("Naviagation",["Dashboard","RETM Readings Update","KPS1 Readings Update"])
 
     if page == "Dashboard":
         dashboard_page()
     if page =="RETM Readings Update":
          retm_readings()
-   
+    if page == "KPS1 Readings Update":
+        kps1_readings()
     if st.button("Logout"):
         st.session_state.clear()
         st.rerun()
